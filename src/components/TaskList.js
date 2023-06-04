a838eca32ba6955a55425e5597e1b86882deea5a
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-06-04 18:53:44 -0400

    complete lab

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+function TaskList({ tasks, onHandleDelete }) {
+  const tasksEl = tasks.map((task) => (
+    <Task
+      onHandleDelete={onHandleDelete}
+      key={task.text}
+      category={task.category}
+      text={task.text}
+    />
+  ));
+  return <div className="tasks"> {tasksEl} </div>;
 }
 
 export default TaskList;
